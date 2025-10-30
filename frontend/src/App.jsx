c5d3a23e40961926a35d5881b9f64627392e4574
ahnsangha <162676127+ahnsangha@users.noreply.github.com>
2025-10-30 18:15:06 +0900

    css 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${!user ? 'auth-mode' : ''}`}>
       <Toaster position="top-center" />
       <Routes>
         <Route
